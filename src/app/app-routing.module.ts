61009a6219dd6c4cfe99c2ceca0b6362f3430d0b
Eric Luc <luckhoi89@gmail.com>
2020-11-29 18:46:30 -0800

    tested router again

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './pages/home/home.component'
+import { MenuComponent } from './pages/menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'home', pathMatch:'full'},
+  {path:'home',component:HomeComponent},
+  {path:'menu',component:MenuComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
