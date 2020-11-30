8275ce80f464918afbb2f37d40f9011e6874582d
Eric Luc <luckhoi89@gmail.com>
2020-11-29 21:46:28 -0800

    layout added

--- src/app/menu/menu.module.ts
+++ src/app/menu/menu.module.ts
@@ -3,18 +3,30 @@ import { CommonModule } from '@angular/common';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzTabsModule } from 'ng-zorro-antd/tabs';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzListModule } from 'ng-zorro-antd/list';
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     NzCardModule,
     NzTabsModule,
-    NzIconModule
+    NzIconModule,
+    NzLayoutModule,
+    NzBreadCrumbModule,
+    NzMenuModule,
+    NzListModule
   ],
   exports:[
     NzCardModule,
     NzTabsModule,
-    NzIconModule
+    NzIconModule,
+    NzLayoutModule,
+    NzBreadCrumbModule,
+    NzMenuModule,
+    NzListModule
   ]
 })
 export class MenuModule { }
