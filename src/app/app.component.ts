d024e59bdcfe1f2d3b4fc9a31ea688b9b3d3b8f3
Eric Luc <luckhoi89@gmail.com>
2020-11-29 22:02:26 -0800

    more things

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private router: Router) {}
   isCollapsed = false;
   title = 'yumeMenuVersion2';
+  isMenuRoute() {
+    return this.router.url === '/menu';
+  }
+  isHomeRoute() {
+    return this.router.url === '/home';
+  }
 }
