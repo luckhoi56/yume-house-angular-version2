49b5477da5423682357c9a45f8ad4c44f0ac27fc
Eric Luc <luckhoi89@gmail.com>
2020-12-14 10:00:33 -0800

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './pages/home/home.component'
 import { MenuComponent } from './pages/menu/menu.component';
-
+import {CheckoutComponent} from './pages/checkout/checkout.component'
 const routes: Routes = [
   {path:'',redirectTo:'home', pathMatch:'full'},
   {path:'home',component:HomeComponent},
-  {path:'menu',component:MenuComponent}
+  {path:'menu',component:MenuComponent},
+  {path:'checkout',component:CheckoutComponent}
 ];
 
 @NgModule({
