e2f737fd24fad43e9cc6b9e9be257b8748d10335
Eric Luc <luckhoi89@gmail.com>
2020-12-14 18:57:13 -0800

    total works now

--- src/app/components/yume-pay/yume-pay.component.ts
+++ src/app/components/yume-pay/yume-pay.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-yume-pay',
@@ -6,8 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./yume-pay.component.css']
 })
 export class YumePayComponent implements OnInit {
-  m_tax = 0
-  m_total = 0
+  @Input() m_tax = 5
+  @Input() m_total = 0
   constructor() { }
 
   ngOnInit(): void {
